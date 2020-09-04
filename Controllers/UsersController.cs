553b1a6e97aaf09bdf011849f9555a37b9d41f79
Jose Mª Izquierdo Galiot <igaliot21@gmail.com>
2020-09-04 12:43:17 +0200

    Users - Index View

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SammysAuto.Data;
+
+namespace SammysAuto.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        public UsersController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var users = this.context.ApplicationUsers.ToList();
+            return View(users);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing) this.context.Dispose();
+        }
+    }
+}
